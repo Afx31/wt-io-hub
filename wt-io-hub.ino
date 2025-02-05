93dbd30f38db21c8252425a922564f8e795c1014
Afx31 <post4wjm@gmail.com>
2025-02-05 17:19:31 +1100

    Multiple button inputs now working in parallel

--- wt-io-hub.ino
+++ wt-io-hub.ino
@@ -26,31 +26,42 @@ void setup() {
     mcp2515.reset();
     mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
     mcp2515.setNormalMode();
-    
-    Serial.println("Example: Write to CAN");
 
     // External trigger button
     pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
 }
 
 // External trigger button
-//int buttonState = 0;
-int buttonState = HIGH;
+int btn3State = HIGH;
+int btn4State = HIGH;
+int btn5State = HIGH;
 
 void loop() {
-    int pinValue = digitalRead(3);
+    int pinValue3 = digitalRead(3);
+    int pinValue4 = digitalRead(4);
+    int pinValue5 = digitalRead(5);
     delay(100); // quick and dirty debounce filter
-    
-    // TODO: Attempt1 - logic wasn't right
-    // if (buttonState != pinValue) {
-    //   buttonState = pinValue;
-    //   Serial.println(buttonState);
-    // }
 
-    // TODO: Attempt2 - Working
-    if (buttonState == HIGH && pinValue == LOW) {
-        Serial.println("Button Pressed");
+    // Btn1
+    if (btn3State == HIGH && pinValue3 == LOW) {
+        Serial.println("Button Pressed - 1");
         mcp2515.sendMessage(&canMsgEnableDatalogging);
     }
-    buttonState = pinValue;
+    btn3State = pinValue3;
+
+    // Btn2
+    if (btn4State == HIGH && pinValue4 == LOW) {
+        Serial.println("Button Pressed - 2");
+        //mcp2515.sendMessage(&canMsgEnableDatalogging);
+    }
+    btn4State = pinValue4;
+
+    // Btn3
+    if (btn5State == HIGH && pinValue5 == LOW) {
+        Serial.println("Button Pressed - 3");
+        //mcp2515.sendMessage(&canMsgEnableDatalogging);
+    }
+    btn5State = pinValue5;
 }
